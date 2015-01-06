897c2a170dcd56ec6abc77f99b9c13c8b7410a9b
Artur Skonecki <skoneka@users.noreply.github.com>
2015-01-06 19:16:54 +0100

    add LZMA

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,38 @@ angular.module('yeomanTodoApp')
       'Item 3'
     ];
     $scope.addTodo = function () {
-      $scope.todos.push($scope.todo);
+
+
+      switch ($scope.algorithm.selected) {
+        //LZMA.compress(string, mode, on_finish(result) {}, on_progress(percent) {});
+        //
+        //  LZMA.decompress(byte_array, on_finish(result) {}, on_progress(percent) {});
+        case 'LZW':
+          // Blah
+          var compressed = LZString.compress($scope.inputForm);
+          $scope.encryptForm = compressed;
+          break;
+
+        case 'LZ77':
+          var compressor = new LZ77();
+          var compressed = compressor.compress($scope.inputForm);
+          $scope.encryptForm = compressed;
+          break;
+
+        case 'LZMA':
+          LZMA.compress($scope.inputForm, 1, function on_compress_complete(result) {
+            $scope.$apply(function () {
+              console.log('compresses: ' + result);
+              $scope.encryptForm = result;
+            });
+
+          }, function on_compress_progress_update(percent) {
+            document.title = "Compressing: " + (percent * 100) + "%";
+          });
+          break;
+      }
+      $scope.todos.push($scope.encryptForm);
+
       $scope.todo = '';
       //$scope.todoBlock.set$scope.todo;
     };
@@ -25,43 +56,72 @@ angular.module('yeomanTodoApp')
     };
 
     $scope.$watch('toggle', function(){
-      $scope.toggleTextEncrypt = $scope.toggle ? 'Toggle!' : 'some text';
+      $scope.toggleTextEncrypt = $scope.toggle ? 'Compress' : 'Compress';
 
 
       switch ($scope.algorithm.selected) {
-        case 'one':
+        //LZMA.compress(string, mode, on_finish(result) {}, on_progress(percent) {});
+        //
+        //  LZMA.decompress(byte_array, on_finish(result) {}, on_progress(percent) {});
+        case 'LZW':
           // Blah
           var compressed = LZString.compress($scope.inputForm);
+          $scope.encryptForm = compressed;
           break;
-        case 'two':
-          // Blah
+
+        case 'LZ77':
           var compressor = new LZ77();
           var compressed = compressor.compress($scope.inputForm);
+          $scope.encryptForm = compressed;
+          break;
+
+        case 'LZMA':
+          LZMA.compress($scope.inputForm, 1, function on_compress_complete(result) {
+            $scope.$apply(function () {
+              console.log('compresses: ' + result);
+              $scope.encryptForm = result;
+            });
+
+          }, function on_compress_progress_update(percent) {
+            document.title = "Compressing: " + (percent * 100) + "%";
+          });
           break;
       }
-      $scope.encryptForm = compressed;
-      debugger;
     });
 
     $scope.$watch('decrypt', function(){
-      $scope.toggleTextDecrypt = 'Decrypt';
+      $scope.toggleTextDecrypt = 'Decompress';
 
 
       switch ($scope.algorithm.selected) {
-        case 'one':
+        case 'LZW':
           // Blah
           var compressed = LZString.decompress($scope.encryptForm);
+          $scope.decryptForm = compressed;
           break;
-        case 'two':
+        case 'LZ77':
           var compressor = new LZ77();
           var compressed = compressor.decompress($scope.encryptForm);
+          $scope.decryptForm = compressed;
+          break;
+
+        case 'LZMA':
+          LZMA.decompress($scope.encryptForm, function on_decompress_complete(result) {
+            $scope.$apply(function () {
+              console.log('decompress: ' + result);
+              $scope.decryptForm = result;
+            });
+          }, function on_decompress_progress_update(percent) {
+            /// Decompressing progress code goes here.
+            document.title = "Decompressing: " + (percent * 100) + "%";
+          });
           break;
       }
-      $scope.decryptForm = compressed;
+
       debugger;
     });
 
-    $scope.algorithms=['LZW', 'LZ77', 'three'];
+    $scope.algorithms=['LZW', 'LZ77', 'LZMA'];
   });
 
 
